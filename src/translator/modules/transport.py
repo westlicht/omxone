22c0e96253a99547067bd73e49db2d961669750f
Simon Kallweit <simon.kallweit@intefo.ch>
2010-11-23 01:24:44 +0100

    fixed key mappings

--- src/translator/modules/transport.py
+++ src/translator/modules/transport.py
@@ -1,4 +1,6 @@
 
+import time
+
 from translator.base import Translator
 from translator.factory import TranslatorFactory
 
@@ -12,18 +14,22 @@ class TranslatorTransport(Translator):
         
         self.add_key('live_next_scene', self.__live_next_scene, self.host, int(options['live_next_scene']))
         self.add_key('key_play', self.__key_play, self.controller, int(options['key_play']))
-        self.add_key('key_next_scene', self.__key_next_scene, self.controller, int(options['key_next_scene']))
         self.add_key('key_prev_scene', self.__key_prev_scene, self.controller, int(options['key_prev_scene']))
+        self.add_key('key_next_scene', self.__key_next_scene, self.controller, int(options['key_next_scene']))
 
         self.add_cmd('play', self.host, int(options['note_play']))
-        self.add_cmd('next_scene', self.host, int(options['note_next_scene']))
         self.add_cmd('prev_scene', self.host, int(options['note_prev_scene']))
+        self.add_cmd('next_scene', self.host, int(options['note_next_scene']))
         
     def __live_next_scene(self, key):
+        self.send_cmd('next_scene')
+        time.sleep(0.1)
         self.send_cmd('play')
+        self.send_cmd('prev_scene')
     
     def __key_play(self, key):
         self.send_cmd('play')
+        self.send_cmd('prev_scene')
     
     def __key_next_scene(self, key):
         self.send_cmd('next_scene')
